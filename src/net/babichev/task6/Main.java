e5a3b51590d8707aae74060a7c886b1fdaa34890
REZ1DENT3 <maksim.babichev95@gmail.com>
2015-11-13 22:32:00 +0300

    init task6

---
+++ src/net/babichev/task6/Main.java
@@ -0,0 +1,26 @@
+package net.babichev.task6;
+
+import java.util.Scanner;
+
+/**
+ * 1.	Прямоугольная область задана координатами x1 y1,x2 y2 концов ее диагонали.
+ *
+ *      Область разбита на прямоугольники так, что одна сторона разбита на n, а другая на m отрезков.
+ *
+ *      В этой области задан треугольник вершинами u1 v1, u2 v2, u3 v3.
+ *      Вычислить количество прямоугольников области, в которых лежит хотя бы одна точка треугольника.
+ *
+ *      Выделить искомые прямоугольники.
+ */
+
+class coord { public int x, y; }
+
+public class Main {
+    public static void main(String[] args) {
+        coord D1, D2;
+        coord T1, T2, T3;
+        int n, m;
+
+        Scanner sc = new Scanner(System.in);
+    }
+}
