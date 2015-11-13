30d156390ade54c77e397af63c13832b466cd443
REZ1DENT3 <maksim.babichev95@gmail.com>
2015-11-13 22:02:36 +0300

    task 4 - complete

---
+++ src/net/babichev/task4/Main.java
@@ -0,0 +1,56 @@
+package net.babichev.task4;
+
+/**
+ * 10.	Определить класс самолет, экземпляры которого имеют поля
+ *      марка,
+ *      объем топливного бака,
+ *      максимальная дальность полета.
+ * Определить функции назначения и изменения полей и
+ * функцию вывода на экран.
+ *
+ * Определить производный	 класс – пассажирский самолет,
+ * экземпляры которого имеют поле количество посадочных мест.
+ * Определить функцию вывода на экран.
+ *
+ * Определить функцию переназначения количества мест.
+ */
+
+class airplane extends Object {
+    protected float maxRange = 5500;
+    protected String stamp = "Airbus A320";
+    protected int fuel;
+
+    public final float getMaxRange() { return this.maxRange; }
+    public final void setMaxRange(float maxRange) { this.maxRange = maxRange; }
+    public final void printMaxRange() { System.out.println(this.toString() + ": " + this.maxRange); }
+
+    public final String getStamp() { return this.stamp; }
+    public final void setStamp(String stamp) { this.stamp = stamp; }
+    public final void printStamp() { System.out.println(this.toString() + ": " + this.stamp); }
+
+    public final int getFuel() { return this.fuel; }
+    public final void setFuel(int fuel) { this.fuel = fuel; }
+    public final void printFuel() { System.out.println(this.toString() + ": " + this.fuel); }
+
+    public void allDataPrint() { this.printFuel(); this.printMaxRange(); this.printStamp(); }
+}
+
+class passengerPlane extends airplane {
+    protected int places = 330;
+    public final int getPlaces() { return this.places; }
+    public final void setPlaces(int places) { this.places = places; }
+    public final void printPlaces() { System.out.println(this.toString() + ": " + this.places); }
+    @Override
+    public final void allDataPrint() {
+        super.allDataPrint();
+        this.printPlaces();
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        passengerPlane airbus = new passengerPlane();
+        airbus.allDataPrint();
+    }
+}
