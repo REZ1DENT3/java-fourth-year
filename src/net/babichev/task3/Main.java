7175aaf12538e61925385431f45015765442240f
REZ1DENT3 <maksim.babichev95@gmail.com>
2015-11-13 21:33:40 +0300

    task 3 - complete

--- src/net/babichev/task3/Main.java
+++ src/net/babichev/task3/Main.java
@@ -15,44 +15,72 @@ import java.util.Scanner;
  */
 
 abstract class Shape extends Object {
+    protected double a, b;
     public abstract double square();
     public abstract void init(Scanner scanner);
 }
 
 class Circle extends Shape {
     public double square() {
-        return 0;
+        return Math.PI * this.a * this.b;
     }
 
     public void init(Scanner scanner) {
-
+        System.out.println(this.toString());
+        System.out.print("Input r: ");
+        if (scanner.hasNextDouble()) {
+            this.a = scanner.nextDouble();
+        }
+        this.b = this.a;
     }
 }
 
 class Rectangle extends Shape {
-    private double a, b;
     public double square() {
-        return a * b;
+        return this.a * this.b;
     }
 
     public void init(Scanner scanner) {
+        System.out.println(this.toString());
+        System.out.print("Input a: ");
+        if (scanner.hasNextDouble()) {
+            this.a = scanner.nextDouble();
+        }
 
+        System.out.print("Input b: ");
+        if (scanner.hasNextDouble()) {
+            this.b = scanner.nextDouble();
+        }
     }
 }
 
 class Square extends Rectangle {
     public void init(Scanner scanner) {
-
+        System.out.println(this.toString());
+        System.out.print("Input a: ");
+        if (scanner.hasNextDouble()) {
+            this.a = scanner.nextDouble();
+        }
+        this.b = this.a;
     }
 }
 
 class Triangle extends Shape {
     public double square() {
-        return 0;
+        return this.a * this.b / 2;
     }
 
     public void init(Scanner scanner) {
+        System.out.println(this.toString());
+        System.out.print("Input a: ");
+        if (scanner.hasNextDouble()) {
+            this.a = scanner.nextDouble();
+        }
 
+        System.out.print("Input h: ");
+        if (scanner.hasNextDouble()) {
+            this.b = scanner.nextDouble();
+        }
     }
 }
 
@@ -62,17 +90,21 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        double x = 0., y = 0.;
-        if (sc.hasNextDouble()) {
-            x = sc.nextDouble();
-        }
+        Circle circle = new Circle();
+        circle.init(sc);
+        System.out.println("Circle S: " + circle.square());
 
-        if (sc.hasNextDouble()) {
-            y = sc.nextDouble();
-        }
+        Rectangle rectangle = new Rectangle();
+        rectangle.init(sc);
+        System.out.println("Rectangle S: " + rectangle.square());
 
-        System.out.println(x);
-        System.out.println(y);
+        Square square = new Square();
+        square.init(sc);
+        System.out.println("Square S: " + square.square());
+
+        Triangle triangle = new Triangle();
+        triangle.init(sc);
+        System.out.println("Triangle S: " + triangle.square());
 
     }
 }
